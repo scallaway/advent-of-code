62f9ba212cd842e0aece332301f91e5a36c65072
Scott Callaway <github@scottcallaway.co.uk>
2020-12-01 17:27:54 +0000

    Initial commit

---
+++ 2020/day1/part1.js
@@ -0,0 +1,41 @@
+const { readFileSync } = require("fs");
+const { performance } = require("perf_hooks");
+
+// Read in the file into a Map for quick access
+const data = new Map(
+  readFileSync("./input.txt", "utf8")
+    .split("\n")
+    .sort((a, b) => a - b)
+    .map((val) => [val, parseInt(val, 10)])
+);
+
+// Dump it into an Array so we can call `.some` for quick breakout
+const dataArr = Array.from(data);
+
+const target = 2020;
+
+let values = [];
+let time0 = performance.now();
+
+// Iterate through the array of values
+// If we find a solution to our equations in the map, then set those values
+// and break out quickly.
+dataArr.some(([_, numberValue]) => {
+  if (data.get((numberValue - target).toString(10)) !== undefined) {
+    values = [numberValue, numberValue - target];
+    return true;
+  }
+
+  if (data.get((target - numberValue).toString(10)) !== undefined) {
+    values = [numberValue, target - numberValue];
+    return true;
+  }
+});
+
+console.log("time taken: ", performance.now() - time0);
+console.log("values: ", values);
+
+console.log(
+  "solution: ",
+  values.reduce((val, acc) => val * acc, 1)
+);
